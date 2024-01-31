0d524aea1e935940cc47f798049cdea84ce6c547
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2024-01-31 19:14:09 -0300

    Fim do Dia 2 Aula 2

--- fe_lab_clinicas_self_service/lib/src/repositories/user/user_repository_impl.dart
+++ fe_lab_clinicas_self_service/lib/src/repositories/user/user_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
@@ -14,13 +15,18 @@ class UserRepositoryImpl implements UserRepository {
   @override
   Future<Either<AuthException, String>> login(String email, String password) async {
     try{
-      final Response(data: {'access_token': access_token}) = await restClient.unauth.post('/auth', data: {
+      final Response(data: {'access_token': accessToken}) = await restClient.unauth.post('/auth', data: {
         'email': email,
         'password': password,
         'admin': true,
       });
+      return Right(accessToken);
     } on DioException catch(e, s) {
       log("Erro ao realizar login", error: e, stackTrace: s);
+      return switch(e) {
+        DioException(response: Response(statusCode: HttpStatus.forbidden)?) => Left(AuthUnauthorizedException()),
+        _ => Left(AuthError(message: 'Erro ao realizar login'))
+      };
     }
   }
   
