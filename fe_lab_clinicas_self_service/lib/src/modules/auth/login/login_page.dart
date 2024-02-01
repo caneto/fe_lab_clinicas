daf6c57bea463bb028467a6aabb51dce12f6213c
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2024-02-01 18:28:53 -0300

    Finalizando Dia 2 Aula 3

--- fe_lab_clinicas_self_service/lib/src/modules/auth/login/login_page.dart
+++ fe_lab_clinicas_self_service/lib/src/modules/auth/login/login_page.dart
@@ -1,14 +1,63 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
 
-  const LoginPage({ super.key });
+  @override
+  Widget build(BuildContext context) {
+    final sizeOf = MediaQuery.sizeOf(context);
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text(''),),
-           body: Container(),
-       );
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          constraints: BoxConstraints(minHeight: sizeOf.height),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/background_login.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Container(
+              padding: const EdgeInsets.all(40),
+              constraints: BoxConstraints(maxWidth: sizeOf.width * .8),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(16),
+                ),
+              ),
+              child: Column(
+                children: [
+                  const Text(
+                    'Login',
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(label: Text('Email')),
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(label: Text('Email')),
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  SizedBox(
+                      width: sizeOf.width * .8,
+                      height: 48,
+                      child: ElevatedButton(
+                          onPressed: () {}, child: const Text('ENTRAR')))
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
