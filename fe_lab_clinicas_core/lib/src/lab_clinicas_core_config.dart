40dac48196a3fc676c96972b6bd341bdda261797
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2024-02-07 18:13:18 -0300

    Fim do Dia 4 Aula 2

--- fe_lab_clinicas_core/lib/src/lab_clinicas_core_config.dart
+++ fe_lab_clinicas_core/lib/src/lab_clinicas_core_config.dart
@@ -13,6 +13,7 @@ class LabClinicasCoreConfig extends StatelessWidget {
     this.pagesBuilders,
     this.modules,
     required this.title,
+    this.didStart,
   });
 
   final ApplicationBindings? binding;
@@ -20,6 +21,7 @@ class LabClinicasCoreConfig extends StatelessWidget {
   final List<FlutterGetItPageBuilder>? pagesBuilders;
   final List<FlutterGetItModule>? modules;
   final String title;
+  final VoidCallback? didStart;
 
   @override
   Widget build(BuildContext context) {
--- fe_lab_clinicas_self_service/lib/main.dart
+++ fe_lab_clinicas_self_service/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:developer';
 
+import 'package:camera/camera.dart';
 import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
 import 'package:fe_lab_clinicas_self_service/src/binding/lab_clinicas_application_binding.dart';
 import 'package:fe_lab_clinicas_self_service/src/modules/auth/auth_module.dart';
@@ -10,8 +11,12 @@ import 'package:fe_lab_clinicas_self_service/src/pages/splash_page/splash_page.d
 import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
 
+late List<CameraDescription> _cameras;
+
 void main() {
-  runZonedGuarded(() {
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    _cameras = await availableCameras();
     runApp(const LabClinicasSelfServiceApp());
   }, (error, stack) {
     log('Erro não tratado', error: error, stackTrace: stack);
@@ -31,6 +36,11 @@ class LabClinicasSelfServiceApp extends StatelessWidget {
         FlutterGetItPageBuilder(page: (_) => const SplashPage(), path: '/'),
       ],
       modules: [AuthModule(), HomeModule(), SelfServiceModule()],
+      didStart: (){
+        FlutterGetItBindingRegister.registerPermanentBinding('CAMERAS', [
+          Bind.lazySingleton((i) => _cameras),
+        ]);
+      },
     );
   }
 }
