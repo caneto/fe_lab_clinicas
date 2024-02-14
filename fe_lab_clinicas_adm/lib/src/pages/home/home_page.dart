bff4e2708a9b6f8648c56781b46edf4be130f2d8
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2024-02-14 17:59:39 -0300

    Finalizado Dia 5 Aula 6

--- fe_lab_clinicas_adm/lib/src/pages/home/home_page.dart
+++ fe_lab_clinicas_adm/lib/src/pages/home/home_page.dart
@@ -1,8 +1,24 @@
 import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
 import 'package:flutter/material.dart';
+import 'package:validatorless/validatorless.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _formKey = GlobalKey<FormState>();
+  final _deskNumberEC = TextEditingController();
+
+  @override
+  void dispose() {
+    _deskNumberEC.dispose();
+    //effectCleanup();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,21 +26,68 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: LabClinicasAppBar(),
       body: Center(
-        child: Container(
-          width: sizeOf.width * .4,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(16),
-            border: Border.all(color: LabClinicasTheme.orangeColor),
-          ),
-          child: const Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text('Bem-Vindo', style: LabClinicasTheme.titleStyle,)
-            ],
+        child: Form(
+          key: _formKey,
+          child: Container(
+            padding: const EdgeInsets.all(40),
+            width: sizeOf.width * .5,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(color: LabClinicasTheme.orangeColor),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  'Bem-Vindo!',
+                  style: LabClinicasTheme.titleStyle,
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                const Text(
+                  'Preencha o número do guichê que você está atendendo',
+                  style: LabClinicasTheme.subTitleSmallStyle,
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                AppDefaultTextformfield(
+                  title: "Numero do guiche",
+                  hintText: 'Numero do guiche',
+                  controller: _deskNumberEC,
+                  onFieldSubmitted: (_) => _enterButton(),
+                  validator: Validatorless.multiple([
+                    Validatorless.required('Guichê obrigatório'),
+                    Validatorless.number('Guichê deve ser um número')
+                  ]),
+                  keyboardType: TextInputType.number,
+                ),
+                const SizedBox(
+                   height: 32,
+                ),
+                AppDefaultEspecialButton(
+                  tipoBotao: true,
+                  onPressed: _enterButton,
+                  label: 'CHAMAR PRÓXIMO PACIENTE',
+                  width: sizeOf.width * .8,
+                  height: 48,
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  void _enterButton() {
+    final formValid = _formKey.currentState?.validate() ?? false;
+
+    if (formValid) {
+      FocusScope.of(context).unfocus();
+      //   controller.login(_emailEC.text, _passwordEC.text);
+    }
   }
 }
