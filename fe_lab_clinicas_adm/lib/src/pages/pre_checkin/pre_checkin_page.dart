f78012a2e54bde1b9db2abf03e51d544cffe7df0
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2024-02-19 17:20:40 -0300

    Continuação do Dia 5 Aula 12

--- fe_lab_clinicas_adm/lib/src/pages/pre_checkin/pre_checkin_page.dart
+++ fe_lab_clinicas_adm/lib/src/pages/pre_checkin/pre_checkin_page.dart
@@ -1,14 +1,35 @@
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
 import 'package:flutter/material.dart';
 
 class PreCheckinPage extends StatelessWidget {
+  const PreCheckinPage({super.key});
 
-  const PreCheckinPage({ super.key });
+  @override
+  Widget build(BuildContext context) {
+    final sizeOf = MediaQuery.sizeOf(context);
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Pre Chckin'),),
-           body: Container(),
-       );
+    return Scaffold(
+      appBar: LabClinicasAppBar(),
+      body: SingleChildScrollView(
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            padding: const EdgeInsets.all(32),
+            margin: const EdgeInsets.only(top: 18),
+            constraints: BoxConstraints(maxWidth: sizeOf.width * .85),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(color: LabClinicasTheme.orangeColor),
+            ),
+            child: Column(
+              children: [
+                Image.asset('assets/images/patient.png')
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
